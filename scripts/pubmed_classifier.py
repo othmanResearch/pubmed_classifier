807831ec1e0b2947050d5c62d3c9da8f13ca5766
hothman <houcemoo@gmail.com>
2025-08-07 17:13:00 +0200

    reading json annotation file from BERN2 in the pipline

--- scripts/pubmed_classifier.py
+++ scripts/pubmed_classifier.py
@@ -1,18 +1,19 @@
-
 from metaflow import FlowSpec, step, Config
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))    # set before calling internal modules
 from utils import read_input
+import inspect 
+
 
 class processData(FlowSpec):
 
-    config_process_data = Config("preprocess", required = True)
-    print(config_process_data["negative"])
+    config = Config("preprocess", required = True)
+    
     @step
     def start(self):
-
-        self.myvar = "hello"
+        """ reads the path to json file  container directory into self.dataset"""
+        self.dataset = read_input.scan_json_path( self.config.bern2_dataset)
         self.next(self.end)
 
 
@@ -21,6 +22,7 @@ class processData(FlowSpec):
         pass
 
 if __name__=="__main__":
+    
     processData()
 
 
--- src/utils/read_input.py
+++ src/utils/read_input.py
@@ -1,10 +1,24 @@
 import json
+import os
 
 
-
-
-def read_bern2_json(filepath):
+def read_bern2_json(filepath) -> list:
+    """
+    read a json file containing one or multiple tagged elements
+    """
     with open(filepath, 'r') as f:
         data = json.load(f)
-    print(data)
+    return data
+
+def scan_json_path(jsondir) -> list:
+    """
+    scans a directory containing json BERN2 nnotated files and return one list
+    containing all the annotations
+    """
+    all_entries = []
+    for json in os.scandir(jsondir):
+        list_of_entries = read_bern2_json(json)
+        all_entries.extend(list_of_entries)
+    return all_entries
+
 
