66db3b87bbdb867f1a19e0455b6c8358a585fa7f
hothman <houcemoo@gmail.com>
2025-08-07 11:50:19 +0200

    set configuration for processData

--- scripts/pubmed_classifier.py
+++ scripts/pubmed_classifier.py
@@ -1,16 +1,17 @@
 
-from metaflow import FlowSpec, step
+from metaflow import FlowSpec, step, Config
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))    # set before calling internal modules
 from utils import read_input
-
-
 
 class processData(FlowSpec):
 
+    config_process_data = Config("preprocess", required = True)
+    print(config_process_data["negative"])
     @step
     def start(self):
+
         self.myvar = "hello"
         self.next(self.end)
 
